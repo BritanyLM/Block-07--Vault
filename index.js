10fded209c1810d96d56104e01380564cca377e9
BritanyLM <britanylm@gmail.com>
2024-10-10 09:03:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+alert ('You have received this message because you have been chosen to open an important vault. Here is the secret combination');
+
+const x=5;
+const y=5;
+const z=x+y;
+const a=50;
+const b=10;
+const c=a-b;
+const lastdigit=39;
+
+alert (`${z},${c},${lastdigit}`);
+
